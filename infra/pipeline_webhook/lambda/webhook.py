31b60f3c8c5d0973010bf025ebc4823367d4dd11
Ken Jenney <ken@Kens-MacBook-Pro.local>
2021-10-03 22:48:56 -0400

    Troubleshooting Payload

--- infra/pipeline_webhook/lambda/webhook.py
+++ infra/pipeline_webhook/lambda/webhook.py
@@ -2,15 +2,14 @@ import json
 import os
 
 def handler(event, context):
-    #print('## PULL REQUEST')
-    #print(event)
+    print('## PULL REQUEST')
+    print(event)
     #for k in event:
     #    print(k)
     #print(event.keys())
     #print(event['version'])
-    json_dict = json.load(event)
-    print('## PULL REQUEST')
-    print(json_dict['body']['pull_request'])
+    #print('## PULL REQUEST')
+    print(event['body']['pull_request'])
     #print(event['body'][0]['action'])
     # If the Pull Request is not closed - let's do something
     # if event['body']['pull_request']['closed_at'] != 'null':
