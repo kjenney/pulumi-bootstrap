91e98a1f4ccca0a02269d40dc2aecec72ef57d61
Ken Jenney <ken@Kens-MacBook-Pro.local>
2021-10-03 23:50:37 -0400

    Troubleshooting Payload

--- infra/pipeline_webhook/lambda/webhook.py
+++ infra/pipeline_webhook/lambda/webhook.py
@@ -2,6 +2,8 @@ import json
 import os
 import boto3
 
+client = boto3.client('codebuild')
+
 def handler(event, context):
     body = event['body']
     body = json.loads(body)
@@ -10,8 +12,10 @@ def handler(event, context):
         # If the Pull Request is merged Zip up the source to S3 via CodeBuild else Lint the code
         if body['pull_request']['merged_at']:
             print('Kick off CodeBuild S3 Copy')
+            #response = client.start_build(projectName='string')
         else:
             print('Kick off CodeBuild Lint')
+            #response = client.start_build(projectName='string')
     return {
         "statusCode": 200,
         "body": json.dumps(event)
