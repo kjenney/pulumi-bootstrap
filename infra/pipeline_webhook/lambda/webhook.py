71b6e12ad6e208f2f56ac57bb377c56329db7dca
Ken Jenney <ken@Kens-MacBook-Pro.local>
2021-10-03 22:17:13 -0400

    Troubleshooting Payload

--- infra/pipeline_webhook/lambda/webhook.py
+++ infra/pipeline_webhook/lambda/webhook.py
@@ -5,14 +5,14 @@ def handler(event, context):
     print('## EVENT')
     print(event)
     print(event['version'])
-    print(event['body']['action'])
+    print(event['body'][0]['action'])
     # If the Pull Request is not closed - let's do something
-    if event['body']['pull_request']['closed_at'] != 'null':
-        # If the Pull Request is merged Zip up the source to S3 via CodeBuild else Lint the code
-        if event['body']['pull_request']['merged_at'] != 'null':
-            print('Kick off CodeBuild S3 Copy')
-        else:
-            print('Kick off CodeBuild Lint')
+    # if event['body']['pull_request']['closed_at'] != 'null':
+    #     # If the Pull Request is merged Zip up the source to S3 via CodeBuild else Lint the code
+    #     if event['body']['pull_request']['merged_at'] != 'null':
+    #         print('Kick off CodeBuild S3 Copy')
+    #     else:
+    #         print('Kick off CodeBuild Lint')
     return {
         "statusCode": 200,
         "body": json.dumps(event)
