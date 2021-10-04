826d9d41f1134b513b0377e2bffa47f18bc8754c
Ken Jenney <ken@Kens-MacBook-Pro.local>
2021-10-03 21:43:25 -0400

    Process based on Pull Request status

--- infra/pipeline_webhook/lambda/webhook.py
+++ infra/pipeline_webhook/lambda/webhook.py
@@ -4,6 +4,13 @@ import os
 def handler(event, context):
     print('## EVENT')
     print(event)
+    # If the Pull Request is not closed - let's do something
+    if event['closed_at'] != 'null':
+        # If the Pull Request is merged Zip up the source to S3 via CodeBuild else Lint the code
+        if event['merged_at'] != 'null':
+            print('Kick off CodeBuild S3 Copy')
+        else:
+            print('Kick off CodeBuild Lint')
     return {
         "statusCode": 200,
         "body": json.dumps(event)
