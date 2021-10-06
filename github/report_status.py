af82ff1313acc61645472413929d943b028ca8f9
Ken Jenney <ken@Kens-MacBook-Pro.local>
2021-10-06 17:08:41 -0400

    Debug reporting status

--- github/report_status.py
+++ github/report_status.py
@@ -3,10 +3,12 @@ import sys
 from github import Github
 
 token = os.environ.get('GITHUB_TOKEN')
-commit_sha = os.environ.get('commit-sha')
+sha = os.environ.get('commit-sha')
 state = sys.argv[1]
 description = sys.argv[2]
 
+print(sha)
+
 g = Github(token)
 repo = g.get_repo("kjenney/pulumi-bootstrap")
-repo.get_commit(sha=commit_sha).create_status(state=state,description=description)
+repo.get_commit(sha=sha).create_status(state=state,description=description)
