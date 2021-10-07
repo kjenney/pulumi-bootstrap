0158c5ef1c04afe7bc1857da9fc438665f904bca
Ken Jenney <kjenney@gmail.com>
2021-10-07 00:00:37 -0400

    Missing oxford comma (#10)
    🤞

--- infra/pipeline-webhook/lambda/webhook.py
+++ infra/pipeline-webhook/lambda/webhook.py
@@ -71,7 +71,7 @@ def buildspec_main(environ):
                 'build': {
                     'commands': [
                         'cd pulumi-bootstrap',
-                        'pip install -r requirements.txt'
+                        'pip install -r requirements.txt',
                         "pylint $(git ls-files '*.py')"
                     ]
                 }
